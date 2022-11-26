0ef84aea488d207734e8c9fe3e651909151149da
bharathaladandi <bharathaladandi@gmail.com>
2022-11-26 20:56:30 +0530

    Some Changes

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require("express");
 const express = require("express");
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080, async () => {
+    try {
+        await connection
+    } catch (err) {
+        console.log("error while connecting to db", err);
+    }
+    console.log("server running on port 8080");
+})
